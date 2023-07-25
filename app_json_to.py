72e75a19a32071b21e5ec9dbb888511aeb136c20
marceÃlo bastos <marceloc.bastos@hotmail.com>
2023-07-25 16:14:55 -0300

    Implement caching for API data retrieval and dataframe generation Add conditional download buttons for Excel and CSV files Introduce Streamlit Session State to maintain state between reruns Improve user feedback with loading messages and error handling Include additional user guidance through text and markdown elements

--- app_json_to.py
+++ app_json_to.py
@@ -5,6 +5,7 @@ import pandas as pd
 import streamlit as st
 
 # Criar função para ler um JSON
+@st.cache_data  # Adicionando cache à função
 def read_json(url):
     response = requests.get(url)
     if response.status_code == 200:
@@ -16,7 +17,7 @@ def read_json(url):
 def visualize_dataframe(data):
     if 'resultado' in data:
         df = pd.DataFrame(data['resultado'])
-        st.dataframe(df.head(10))
+        st.session_state.dataframe = df
     else:
         st.warning("Nenhum dado disponível para visualização.")
 
@@ -24,8 +25,7 @@ def visualize_dataframe(data):
 def export_to_excel(data):
     if 'resultado' in data:
         df = pd.DataFrame(data['resultado'])
-        with pd.ExcelWriter("temp.xlsx") as writer:
-            df.to_excel(writer, index=False)
+        df.to_excel("temp.xlsx", index=False)
         return True
     else:
         st.warning("Nenhum dado disponível para exportar.")
@@ -42,67 +42,67 @@ def export_to_csv(data):
         return False
 
 # Layout Streamlit
-st.title("JSON para Excel")
+st.title("JSON para Excel ou CSV")
+
+# Nota de descrição
+st.markdown("Esta aplicação permite a visualização e exportação de dados da "
+            "[API Compras Governamentais](https://api-comprasv2.dth.nuvem.gov.br/swagger-ui/index.html#/) "
+            "do governo brasileiro para formatos Excel e CSV, facilitando a análise subsequente desses dados.")
+
 
 # Campo de pesquisa
 url = st.text_input("Qual é a url da API?")
-st.text("Exemplo de URL: api... ")
+st.text("Exemplo de URL: ")
 st.text("https://api-comprasv2.dth.nuvem.gov.br/modulo-pesquisa-preco/1_consultarMaterial?pagina=1&codigoItemCatalogo=267203")
 
-# Variável de controle para exibir o botão de download após a exportação
-exported_excel = False
-exported_csv = False
+st.divider()
+
+# Initialising the session_state variables for excel and csv
+if "exported_excel" not in st.session_state:
+    st.session_state.exported_excel = False
+if "exported_csv" not in st.session_state:
+    st.session_state.exported_csv = False
+if "dataframe" not in st.session_state:
+    st.session_state.dataframe = pd.DataFrame()
 
 # Botão de pesquisa
-if st.button("Visualizar", key="visualizar_button"):
+if st.button(" :sunglasses: Visualizar Prévia ", key="visualizar_button"):
     if url:
         try:
-            st.text("Carregando dados...")
+            # Use st.empty() para criar um espaço vazio no app
+            message = st.text("Carregando dados...")
+
             data = read_json(url)
             visualize_dataframe(data)
+
+            # Exportando dados para Excel e CSV
+            st.session_state.exported_excel = export_to_excel(data)
+            st.session_state.exported_csv = export_to_csv(data)
+
+            # Remova a mensagem "Carregando dados..." usando a função empty()
+            message.empty()
+
         except Exception as e:
             st.error("Ocorreu um erro ao recuperar os dados da API.")
             st.error(e)
+    else:
+        st.warning("Por favor, informe uma URL.")  # Adicionando aviso quando a URL estiver vazia
 
-# Botão de exportação para Excel
-if st.button("Exportar para Excel", key="exportar_excel_button"):
-    if url:
-        try:
-            st.text("Exportando para Excel...")
-            with st.spinner("Exportando... Aguarde o download do arquivo."):
-                data = read_json(url)
-                if export_to_excel(data):
-                    exported_excel = True
-                    st.success("Dados exportados com sucesso! Clique abaixo para baixar o arquivo Excel.")
-        except Exception as e:
-            st.error("Ocorreu um erro ao exportar os dados para Excel.")
-            st.error(e)
-
-# Botão de exportação para CSV
-if st.button("Exportar para CSV", key="exportar_csv_button"):
-    if url:
-        try:
-            st.text("Exportando para CSV...")
-            with st.spinner("Exportando... Aguarde o download do arquivo."):
-                data = read_json(url)
-                if export_to_csv(data):
-                    exported_csv = True
-                    st.success("Dados exportados com sucesso! Clique abaixo para baixar o arquivo CSV.")
-        except Exception as e:
-            st.error("Ocorreu um erro ao exportar os dados para CSV.")
-            st.error(e)
+# Display the dataframe if it exists
+if not st.session_state.dataframe.empty:
+    st.dataframe(st.session_state.dataframe.head(6))
 
 # Botões de download condicional
-if exported_excel:
+if st.session_state.exported_excel:
     st.download_button(
-        label="Clique aqui para baixar o arquivo Excel",
+        label="Exportar para Excel",
         data=open("temp.xlsx", "rb"),
         file_name="dataframe_exportado.xlsx",
     )
 
-if exported_csv:
+if st.session_state.exported_csv:
     st.download_button(
-        label="Clique aqui para baixar o arquivo CSV",
+        label="Exportar para CSV",
         data=open("temp.csv", "rb"),
         file_name="dataframe_exportado.csv",
     )
